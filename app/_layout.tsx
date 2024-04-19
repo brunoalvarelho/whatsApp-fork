3967e7f7ed52a9049002d56afddaaef3287e2c46
brunoalvarelho <38031172+brunoalvarelho@users.noreply.github.com>
2024-04-20 01:38:52 +0200

    add new chat modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack, useRouter, useSegments } from 'expo-router';
+import { Link, Stack, useRouter, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
 import * as SecureStore from 'expo-secure-store';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const EXPO_CLERK_PUBLISHABLE_API = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
@@ -66,6 +68,24 @@ const InitialLayout = () => {
       <Stack.Screen name="otp" options={{ headerTitle: "Enter Your Phone Number", headerBackVisible: false }} />
       <Stack.Screen name="verify/[phone]" options={{ headerTitle: "Verify Phone Number", headerBackTitle: 'Edit number' }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(modals)/new-chat" options={{
+        presentation: 'modal',
+        title: 'New Chat',
+        headerTransparent: true,
+        headerBlurEffect: 'regular',
+        headerStyle: { backgroundColor: Colors.background },
+        headerSearchBarOptions: {
+          placeholder: 'Search name or number...',
+          hideWhenScrolling: false,
+        },
+        headerRight: () => (
+          <Link href="/(tabs)/chats" asChild>
+            <TouchableOpacity style={{ backgroundColor: Colors.lightGray, padding: 4, borderRadius: 20 }}>
+              <Ionicons name="close" size={30} color={Colors.gray} />
+            </TouchableOpacity>
+          </Link>
+        ),
+      }} />
     </Stack>
   );
 }
